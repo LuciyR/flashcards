3f1567600760d91886f3c65b7c1230a550137c8a
LuciyR <obraz.org@gmail.com>
2017-01-23 22:40:51 +0200

    Fix test strings

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get main page" do
+  test 'should get main page' do
     get root_url
     assert_response :success
   end
