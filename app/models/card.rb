f22644ef8c2faaae952df7740919a841032fc3ca
LuciyR <obraz.org@gmail.com>
2017-01-26 11:43:59 +0200

    Update card model validation and callbacks

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,16 +1,16 @@
 class Card < ApplicationRecord
-  after_initialize :init
-  validates_presence_of :original_text, :translated_text, :review_date
+  before_create :set_review_date
+  validates_presence_of :original_text, :translated_text
   validate :cards_text_cannot_match
 
   protected
 
-  def init
+  def set_review_date
     self.review_date = Time.now + 3.days
   end
 
   def cards_text_cannot_match
-    if original_text.capitalize == translated_text.capitalize
+    if original_text.downcase == translated_text.downcase
       errors.add(:translated_text, 'Перевод должен отличаться от оригинала')
     end
   end
