f204368dac10b1a5926bbe03f77176e501129924
LuciyR <obraz.org@gmail.com>
2017-02-06 09:11:09 +0200

    Refactor feature test

--- spec/features/check_user_answers_spec.rb
+++ spec/features/check_user_answers_spec.rb
@@ -1,27 +1,29 @@
 require 'rails_helper'
 Capybara.ignore_hidden_elements = false
 
-RSpec.feature 'Check user answers', type: :feature do
+RSpec.feature 'Main Page', type: :feature do
   before do
     @card = create(:card)
     @card.update_attributes(review_date: Time.now - 4.days)
     visit root_url
   end
 
-  scenario 'We have form with random card on main page' do
+  scenario 'render random card' do
     expect(page).to have_content(@card.translated_text)
   end
 
-  scenario 'Check correct answer' do
-    fill_in 'answer', with: @card.original_text
-    click_button 'Готово'
-    expect(page).to have_content('Верно!')
-  end
+  feature 'filling form' do
+    scenario 'correct answer' do
+      fill_in 'answer', with: @card.original_text
+      click_button 'Готово'
+      expect(page).to have_content('Верно!')
+    end
 
-  scenario 'Check wrong answer' do
-    fill_in 'answer', with: 'RoR'
-    click_button 'Готово'
-    expect(page).
-      to have_content("Вы ошиблись. Правильный ответ: #{@card.original_text}")
+    scenario 'wrong answer' do
+      fill_in 'answer', with: 'RoR'
+      click_button 'Готово'
+      expect(page).
+        to have_content("Вы ошиблись. Правильный ответ: #{@card.original_text}")
+    end
   end
 end
